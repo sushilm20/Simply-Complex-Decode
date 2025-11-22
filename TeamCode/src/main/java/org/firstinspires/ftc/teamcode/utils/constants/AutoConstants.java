c5da079f6b70cba1353a99e016723cbb48f938d9
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-11-22 17:19:03 -0500

    delete stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/AutoConstants.java
@@ -1,22 +1,23 @@
-package org.firstinspires.ftc.teamcode.pedroPathing.constants;
+package org.firstinspires.ftc.teamcode.utils.constants;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.localization.Pose;
 
 @Config
 public class AutoConstants {
-    public static double shootingAngle = 135;
+    public static double shootingAngle = 180;
     public static int firstWait = 300;
     public static int secondWait = 700;
     public static int thirdWait = 1800;
     public static double shootingX = 60;
     public static double shootingY = 84;
-    public static double firstIntakeX = 16;
+    public static double firstIntakeX = 15;
     public static double secondIntakeX = 10.000;
     public static Pose finalPose = new Pose(0,0,0);
     public static int intakeBallWait = 200;
     public static double leverPoseX = 33.34;
-    public static double leverPoseY = 63.57;
+    public static double leverPoseY = 73;
     public static double startX = 21.32;
     public static double startY = 123.84;
+    public static double leverX = 18.0;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/CloseSideAutoPoseData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/CloseSideAutoPoseData.java
@@ -1,10 +1,11 @@
-package org.firstinspires.ftc.teamcode.pedroPathing.constants;
+package org.firstinspires.ftc.teamcode.utils.constants;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.constants.AutoConstants.startX;
-import static org.firstinspires.ftc.teamcode.pedroPathing.constants.AutoConstants.startY;
+import static org.firstinspires.ftc.teamcode.utils.constants.AutoConstants.startX;
+import static org.firstinspires.ftc.teamcode.utils.constants.AutoConstants.startY;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.localization.Pose;
-
+@Config
 public class CloseSideAutoPoseData {
     // Blue-side coordinate definitions (field center at 72,72, field is 144x144)
     public static final Pose START_POSE     = new Pose(startX, startY, Math.toRadians(143.7));
@@ -13,11 +14,11 @@ public class CloseSideAutoPoseData {
     public static final Pose FIRST_INTAKE   = new Pose(AutoConstants.firstIntakeX, 84.000);
     public static final Pose MID2_CURVE     = new Pose(66, 52);           // path4 control point
     public static final Pose SECOND_INTAKE  = new Pose(AutoConstants.secondIntakeX, 60.000);
-    public static final Pose LEVER = new Pose(16.28, 72);
+    public static final Pose LEVER = new Pose(AutoConstants.leverX, 72);
     public static final Pose LEVER_CONTROL = new Pose(AutoConstants.leverPoseX, AutoConstants.leverPoseY);
     public static final Pose MID3_CURVE     = new Pose(87, 31);           // path6 control point
     public static final Pose FINAL_INTAKE   = new Pose(AutoConstants.secondIntakeX, 33);
-    public static final double START_HEADING  = 143;
+    public static final double START_HEADING  = 143.7;
     public static final double SHOOTING_HEADING = AutoConstants.shootingAngle;
     // Add any more as needed...
 
