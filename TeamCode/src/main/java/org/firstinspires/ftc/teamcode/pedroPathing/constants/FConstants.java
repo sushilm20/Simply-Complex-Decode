5c4dd31b35ef2d26b67991b9f7b8fee4773ffd2d
Rajveer Dharkar <dharkarajveer@gmail.com>
2025-12-27 17:12:01 -0500

    tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/FConstants.java
@@ -20,13 +20,13 @@ public class FConstants {
         FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
         FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
 
-        FollowerConstants.mass = 11.352;
+        FollowerConstants.mass = 13.47;
 
-        FollowerConstants.xMovement = 71.99289;
-        FollowerConstants.yMovement = 56.83860;
+        FollowerConstants.xMovement = 65.92316928678785;
+        FollowerConstants.yMovement = 47.957051860836636;
 
-        FollowerConstants.forwardZeroPowerAcceleration = -40.3737;
-        FollowerConstants.lateralZeroPowerAcceleration = -78.15011779;
+        FollowerConstants.forwardZeroPowerAcceleration = -32.48450558244039;
+        FollowerConstants.lateralZeroPowerAcceleration = -78.64316711329643;
 
         FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.15,0,0.02,0);
         FollowerConstants.useSecondaryTranslationalPID = true;
@@ -40,11 +40,12 @@ public class FConstants {
         FollowerConstants.useSecondaryDrivePID = true;
         FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.01,0,0.0007,0.6,0); // Not being used, @see useSecondaryDrivePID
 
-        FollowerConstants.zeroPowerAccelerationMultiplier = 4;
+        FollowerConstants.zeroPowerAccelerationMultiplier = 6;
         FollowerConstants.centripetalScaling = 0.0006;
 
         FollowerConstants.useBrakeModeInTeleOp = true;
-
+        FollowerConstants.useVoltageCompensationInAuto = true;
+        FollowerConstants.nominalVoltage = 12;
         FollowerConstants.pathEndTimeoutConstraint = 50;
         FollowerConstants.pathEndTValueConstraint = 0.95;
         FollowerConstants.pathEndVelocityConstraint = 0.1;
