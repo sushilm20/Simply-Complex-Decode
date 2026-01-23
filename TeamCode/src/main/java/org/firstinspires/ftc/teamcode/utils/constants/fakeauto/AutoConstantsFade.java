ecd45bcdc9dae6ca02313a254bb90d0036a20cb4
RAllala <78286626+ebdevStudent@users.noreply.github.com>
2026-01-22 21:21:05 -0500

    ts Fade auto Update push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/fakeauto/AutoConstantsFade.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/fakeauto/AutoConstantsFade.java
@@ -1,26 +1,24 @@
 package org.firstinspires.ftc.teamcode.utils.constants.fakeauto;
 
 import com.acmerobotics.dashboard.config.Config;
-
-import com.acmerobotics.dashboard.config.Config;
+import com.pedropathing.localization.Pose;
 
 @Config
 public class AutoConstantsFade {
     public static double finalShootHeading = 225;
     public static double startHeading = 156;
     public static double shootingAngle = 180;
-    public static double leverHeading = 110;
+    public static double leverHeading = 149;
     public static double secondIntakeY = 62;
     public static double shootingX = 64;
     public static double shootingY = 80;
     public static double firstIntakeX = 18;
-    public static double secondIntakeX = 13;
-    public static int leverIntakeBallWait = 300;
-    public static double leverPoseX = 21; //control point
-    public static double leverPoseY = 52; //control point
+    public static double secondIntakeX = 14;
+    public static int leverIntakeBallWait = 1000;
+    public static double leverPoseX = 35; //control point
+    public static double leverPoseY = 59; //control point
     public static double startX = 20.49;
     public static double startY = 124.47;
     public static double leverX = 14;
-    public static double leverY = 64;
-
+    public static double leverY = 61.5;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/fakeauto/CloseSideAutoPoseDataFade.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/fakeauto/CloseSideAutoPoseDataFade.java
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.teamcode.utils.constants.fakeauto;
 
-import static org.firstinspires.ftc.teamcode.utils.constants.auto.AutoConstants.startX;
-import static org.firstinspires.ftc.teamcode.utils.constants.auto.AutoConstants.startY;
+
+import static org.firstinspires.ftc.teamcode.utils.constants.fakeauto.AutoConstantsFade.startX;
+import static org.firstinspires.ftc.teamcode.utils.constants.fakeauto.AutoConstantsFade.startY;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.pedropathing.localization.Pose;
@@ -11,19 +12,19 @@ import org.firstinspires.ftc.teamcode.utils.constants.auto.AutoConstants;
 @Config
 public class CloseSideAutoPoseDataFade {
     public static final Pose START_POSE     = new Pose(startX, startY, Math.toRadians(143.7));
-    public static final Pose SHOOTING_POSE  = new Pose(AutoConstants.shootingX, AutoConstants.shootingY);
+    public static final Pose SHOOTING_POSE  = new Pose(AutoConstantsFade.shootingX, AutoConstantsFade.shootingY);
     public static final Pose MID1_CURVE     = new Pose(52.3, 85);     // path2 control point
-    public static final Pose FIRST_INTAKE   = new Pose(AutoConstants.firstIntakeX, 87.000);
+    public static final Pose FIRST_INTAKE   = new Pose(AutoConstantsFade.firstIntakeX, 87.000);
     public static final Pose MID2_CURVE     = new Pose(66, 52);           // path4 control point
-    public static final Pose SECOND_INTAKE  = new Pose(AutoConstants.secondIntakeX, AutoConstants.secondIntakeY);
-    public static final Pose LEVER = new Pose(AutoConstants.leverX, AutoConstants.leverY);
-    public static final Pose LEVER_CONTROL = new Pose(AutoConstants.leverPoseX, AutoConstants.leverPoseY);
+    public static final Pose SECOND_INTAKE  = new Pose(AutoConstantsFade.secondIntakeX, AutoConstantsFade.secondIntakeY);
+    public static final Pose LEVER = new Pose(AutoConstantsFade.leverX, AutoConstantsFade.leverY);
+    public static final Pose LEVER_CONTROL = new Pose(AutoConstantsFade.leverPoseX, AutoConstantsFade.leverPoseY);
     public static final Pose MID3_CURVE     = new Pose(79, 28);           // path6 control point
-    public static final Pose FINAL_INTAKE   = new Pose(AutoConstants.secondIntakeX, 38.5);
+    public static final Pose FINAL_INTAKE   = new Pose(AutoConstantsFade.secondIntakeX, 38.5);
     public static final Pose FINAL_SHOOT   = new Pose(56, 115);
     public static final Pose LEVER_INTAKE = new Pose(8, 52);
-    public static final double START_HEADING  = AutoConstants.startHeading;
-    public static final double SHOOTING_HEADING = AutoConstants.shootingAngle;
+    public static final double START_HEADING  = AutoConstantsFade.startHeading;
+    public static final double SHOOTING_HEADING = AutoConstantsFade.shootingAngle;
     public static double mirrorX(double x, String color) {
         return color.equals("RED") ? 144 - x : x;
     }
