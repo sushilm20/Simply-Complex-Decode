b2c57fb03f1767c75a687899204b40a3d50b6a2a
KarthikChim <karthikchim784@gmail.com>
2026-02-11 20:08:27 -0500

    AUTO fix
    Co-Authored-By: RAllala <78286626+ebdevStudent@users.noreply.github.com>
    Co-Authored-By: Rajveer Dharkar <153466648+RajDharkar@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/auto/AutoConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/constants/auto/AutoConstants.java
@@ -4,15 +4,16 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class AutoConstants {
+    public static double leverHitHeading = 140;
     public static double finalShootHeading = 225;
     public static double startHeading = 321;
     public static double shootingAngle = 180;
-    public static double leverHeading = 120;
-    public static double secondIntakeY = 62;
+    public static double leverHeading = 140;
+    public static double secondIntakeY = 60;
     public static double shootingX = 64;
     public static double shootingY = 80;
-    public static double firstIntakeX = 18;
-    public static double secondIntakeX = 13;
+    public static double firstIntakeX = 21;
+    public static double secondIntakeX = 16 ;
     public static double leverIntakeX = 13;
     public static double leverIntakeY = 59;
     public static double leverPoseX = 35; //control point
@@ -20,7 +21,7 @@ public class AutoConstants {
     public static double leverWait = 100;
     public static double startX = 18.21;
     public static double startY = 119;
-    public static double leverX = 14;
+    public static double leverX = 13.5;
     public static double leverY = 61.5;
 
 }
